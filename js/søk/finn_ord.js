bc0b76065f7a13101de79927c2e6efe5688fc98c
Fredrik Slungård <FredrikSlungard@users.noreply.github.com>
2018-10-03 11:03:52 +0200

    startet med test av javascript

---
+++ js/søk/finn_ord.js
@@ -0,0 +1,10 @@
+/* Leter gjennom en html fil med tekst og legger til en <a> tag hvis ordet matcher. Den legger kun til slike lenger hvis teksten er en del av en paragraf <p> */
+
+// Finner ordene som skal erstattes i dokumentet
+Last_Inn = () => {
+  let path = '/main/veiledning/sekundær_enhet.html';
+  return path;
+
+}
+
+console.log(Last_Inn);
