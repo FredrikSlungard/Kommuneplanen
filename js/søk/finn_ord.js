2ce5bc48bbc7a6192d9f79e4c1ee1bcf93c187d1
Fredrik Slungård <Fredrik.slungard@outlook.com>
2018-10-04 13:03:22 +0200

    jobbet med funksjon for å finne ord

--- js/søk/finn_ord.js
+++ js/søk/finn_ord.js
@@ -1,10 +1,13 @@
 /* Leter gjennom en html fil med tekst og legger til en <a> tag hvis ordet matcher. Den legger kun til slike lenger hvis teksten er en del av en paragraf <p> */
 
 // Finner ordene som skal erstattes i dokumentet
-Last_Inn = () => {
-  let path = '/main/veiledning/sekundær_enhet.html';
-  return path;
+let doc = $('body')  //'/../main/test/index.html';
+let søk_etter = 'sekundær boenhet';
 
-}
+let new_content = doc.html().replace('/' + søk_etter +'/g', '<b>' + søk_etter + '</b>');
 
-console.log(Last_Inn);
+doc.html(new_content);
+
+
+//$('<b>' + søk_etter + '</b>')  //
+//$('p:contains(' + søk_etter + ')').css('text-decoration' , 'underline');
