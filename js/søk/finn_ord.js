93ff94715a80830846503516e9b951c117d1ee6a
Fredrik Slungård <Fredrik.slungard@outlook.com>
2018-10-04 14:48:06 +0200

    la til tooltip

--- js/søk/finn_ord.js
+++ js/søk/finn_ord.js
@@ -1,19 +1,42 @@
 /* Leter gjennom en html fil med tekst og legger til en <a> tag med hyperlenke hvis ordet matcher. 
 
 Den legger kun til slike lenger hvis teksten er en del av en paragraf <p> */
-
 // Finner ordene som skal erstattes i dokumentet
+Legg_Til_Lenke = (doc) => {
+  doc.each(function () {
+    let para = $(this);
+  
+    if (para.is(":visible")) {
+      para.html(para.html().replace(/sekundær boenhet/ig, '<a href="#sekundær_boenhet" data-toggle="collapse">sekundær boenhet</a>')); 
+    }
+  });
+};
+
+// Legger til en hoverbox over ordet (hvis det ikke er en lenke fra før)
+Add_Hover = (doc) => {
+  doc.each(function () {
+    let para = $(this);
+  
+    if (!$(para).closest('a').length) {
+      para.html(para.html().replace(/hovedbruksenheten/ig, '<a href="#" data-toggle="tooltip"  data-placement="top" title="Hooray!">hovedbruksenheten</a>'));
+    };
+  });
+};
+
+
 let all_para = $('p');
 let søk_etter = 'sekundær boenhet';
 
-all_para.each(function () {
-  let para = $(this);
+Legg_Til_Lenke(all_para);
+Add_Hover(all_para)
 
-  if (para.is(":visible")) {
-    para.html(para.html().replace(/sekundær boenhet/ig, '<a href="#sekundær_boenhet" data-toggle="collapse">sekundær boenhet</a>')); 
-  }
-  console.log("OK");
+
+/* const instance = new Tooltip(referenceElement, {
+  title: "Hey there",
+  trigger: "click",
 });
+instance.hide();
 
-
-//doc.html(doc.html().replace(/sekundær boenhet/ig, '<a href="#sekundær_boenhet" data-toggle="collapse">sekundær boenhet</a>')); 
+var popper = new Popper(referenceElement, onBottomPopper, {
+  placement: 'bottom'
+}); */
