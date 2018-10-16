58f55b60b2bb640623f49d7312deff354cb76f3c
Fredrik Slungård <FredrikSlungard@users.noreply.github.com>
2018-10-16 12:10:00 +0200

    Oppdaterte overskrifter i bestemmelsene

---
+++ js/load.js
@@ -0,0 +1,9 @@
+// Laster inn innholdet fra de ulike html filene som skal inn i indeksen ved oppstart.
+'use strict';
+
+const Best_Fldr = '/main/bestemmelser';
+let Mapper = ['rekkefølgekrav og utbyggingsavtaler', 
+          'kultur, grønt, vann og sikkerhet', 
+          'transport og næring'];
+
+
