673cd359776eaef83f3f9d3070d48bc706c43ca3
Fredrik Slungård <FredrikSlungard@users.noreply.github.com>
2018-10-23 14:11:04 +0200

    Endret mappestruktur

---
+++ main/js/main_content.js
@@ -0,0 +1,30 @@
+// Laster innholdet i main
+$(function() {
+  'user strict';
+
+  let Fldr = '/main/html/bestemmelser/';
+  let Filer = [
+    '1_plankrav',
+    '2_rekkefølgekrav',
+    '3_utbyggingsavtaler',
+    '4_forhold_gjeldende_plan',
+    '5_kulturminne_kulturmiljø',
+    '6_grønnstruktur',
+    '7_vassdrag naturmiljø',
+    '8_strandsone og brygge',
+    '12_transport_parkering',
+    '13_samfunnssikkerhet'
+  ];
+
+  let Path = '';
+  let Content
+
+  // Gå gjennom alle filene som skal lastes inn
+  $.each(Filer, function (index, value) {
+    
+
+  });
+  
+
+
+});
