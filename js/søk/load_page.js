c66b556f69b75d0ef908c2fd39702d7d0cdf0fd4
Fredrik Slungård <38437182+FredrikSlungard@users.noreply.github.com>
2018-10-26 10:31:29 +0200

    Revert "Innhold"

--- js/søk/load_page.js
+++ js/søk/load_page.js
@@ -1,16 +1,15 @@
 /* 
+Script som kjører etter at siden er lastet inn.
 Legger til click event for alle nedtrekkslister (collapsible).
 Listen ekspanderes eller kollapses (avhengig av hvilket item det er)
-Den flyttes til høyre for lenken hvis den ikke allerede er der.
 
 Hvis innholdet er skjult, legges til pluss, ved å nevne begge CSS stylene
 sikrer vi at den enten fjerner collapsible eller visible og legger til vica versa.
 */
-
-$('.collapsible').on('click', function () {
+$('.collapsible').on('click', function() {
 
   let content = $(this).next('.content');
-  
+
   //Denne MÅ komme før slideToggle
   if (content.is(":hidden")) {
     $(this).toggleClass("collapsible visible");
@@ -18,6 +17,7 @@ $('.collapsible').on('click', function () {
   else {
     $(this).toggleClass("collapsible visible");
   }
+
   // Skjuler/Viser innholdet
   content.slideToggle('fast');
 
